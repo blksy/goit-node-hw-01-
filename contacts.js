4f17044184657039c4959f01467a0577b21beb6e
Bartosz <boleksy14@gmail.com>
2022-08-24 18:29:34 +0200

    finished task

--- contacts.js
+++ contacts.js
@@ -1,16 +1,50 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-function listContacts() {
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === contactId);
+  if (idx === -1) {
+    return null;
   }
-  
-  function getContactById(contactId) {
+  const [result] = contacts.splice(idx, 1);
+  await updateContacts(contacts);
+  return result;
+}
 
-  }
-  
-  function removeContact(contactId) {
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
 
-  }
-  
-  function addContact(name, email, phone) {
-
-  }
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
